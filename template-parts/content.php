bdf371e5e8e8796e1caf330258b63e388d030299
Nick Harris <nharris614@gmail.com>
2021-02-07 01:50:24 -0600

    Modify / clean up boilerplate

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,3 +1,4 @@
+<?php if ( !is_front_page() ) : ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-12' ); ?>>
 
 	<header class="entry-header mb-4">
@@ -39,3 +40,28 @@
 	<?php endif; ?>
 
 </article>
+<?php else : ?>
+	<div class="entry-content">
+			<?php
+			/* translators: %s: Name of current post */
+			the_content(
+				sprintf(
+					__( 'Continue reading %s', 'tailpress' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'tailpress' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">, </span>',
+				)
+			);
+			?>
+		</div>
+
+<?php endif; ?>

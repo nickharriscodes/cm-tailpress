87a534f481276203c3d39606ae65552160df5fe4
Nick Harris <nharris614@gmail.com>
2021-02-28 19:06:08 -0600

    Fix blog template

--- single.php
+++ single.php
@@ -11,12 +11,12 @@
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php
+			<!-- <?php
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-			?>
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
+			?> -->
 
 		<?php endwhile; ?>
 
